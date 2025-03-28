16a6dee904a8b7ea16829d076cbd015c612f8e21
nayeem-miah <nayeem5113a@gmail.com>
2025-03-28 23:56:52 +0600

    creating routing

---
+++ server/routeHandler/todoHandler.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+// get all the todos 
+router.get('/', async (req, res) => {
+    
+})
+
+// GET A TODO BY ID
+router.get('/:id', async (req, res) => {
+    
+})
+
+// POST TODO
+router.post("/all", async (req, res) => {
+    
+})
+
+// put todo
+router.put('/:id', async (req, res) => {
+    
+})
+
+// delete todo
+router.delete('/:id', async (req, res) => {
+    
+})
