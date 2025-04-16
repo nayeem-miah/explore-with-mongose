a0e577860cd95993cbce220af5dd68dca8b2163b
nayeem-miah <nayeem5113a@gmail.com>
2025-04-16 10:59:28 +0600

    sign up success

--- server/routeHandler/userHandler.js
+++ server/routeHandler/userHandler.js
@@ -1,23 +1,30 @@
 const express = require("express")
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt")
 const userSchema = require("../schmas/userSchma");
 const router = express.Router();
 const User =new mongoose.model("user", userSchema)
 
 // sign up
 router.post("/signup", async (req, res) => {
-   try {
+    try {
+        const hashedPassword =await bcrypt.hash(req.body.password, 10);
         const newUser = new User({
-
+            name : req.body.name,
+            username : req.body.username,
+            email: req.body.email,
+            password : hashedPassword
         });
         const result = await newUser.save();
-         res.status(200).json({
-        data : result
+        console.log(result);
+        res.status(200).json({
+             message : "sign up success",
+             data : result
     })
    } catch (error) {
        console.error(error)
        res.status(500).json({
-           message : "there was a server site error"
+           message : "sign up failed !!!"
        })
    }
 })
